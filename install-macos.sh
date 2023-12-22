19eede467154430c724beb91802784ba71d7b90f
Vítor Avelino <contact@vitoravelino.me>
2023-12-21 22:26:50 -0300

    add install macos script draft
    Signed-off-by: Vítor Avelino <contact@vitoravelino.me>

---
+++ install-macos.sh
@@ -0,0 +1,24 @@
+brew install coreutils curl git
+
+# install asdf
+
+# copy tool-versions
+
+# asdf install
+
+# asdf reshim
+
+# go install github.com/rhysd/dotfiles@latest
+
+# gnupg
+# https://gist.github.com/phortuin/cf24b1cca3258720c71ad42977e1ba57
+
+brew tap homebrew/cask-fonts
+brew tap FelixKratz/formulae
+brew tap koekeishiya/formulae
+
+brew install fzf gnupg pinentry-mac htop bat wget ripgrep jq eza
+
+brew install dropbox firefox keepassxc visual-studio-code discord slack steam alacritty vlc spotify borders flameshot
+
+chmod +x /Applications/flameshot.app/Contents/MacOS/flameshot
