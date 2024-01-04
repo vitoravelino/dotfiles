39d1bfa5d57ba8cae3a5127214c2c1d8ed35e43a
Vítor Avelino <contact@vitoravelino.me>
2024-01-04 19:36:45 -0300

    update install-macos with more apps/info
    Signed-off-by: Vítor Avelino <contact@vitoravelino.me>

--- install-macos.sh
+++ install-macos.sh
@@ -1,3 +1,7 @@
+brew tap homebrew/cask-fonts
+brew tap FelixKratz/formulae
+brew tap koekeishiya/formulae
+
 brew install coreutils curl git
 
 # install asdf
@@ -13,12 +17,10 @@ brew install coreutils curl git
 # gnupg
 # https://gist.github.com/phortuin/cf24b1cca3258720c71ad42977e1ba57
 
-brew tap homebrew/cask-fonts
-brew tap FelixKratz/formulae
-brew tap koekeishiya/formulae
+brew install fzf gnupg pinentry-mac htop bat wget ripgrep jq eza btop neofetch grep
 
-brew install fzf gnupg pinentry-mac htop bat wget ripgrep jq eza
-
-brew install dropbox firefox keepassxc visual-studio-code discord slack steam alacritty vlc spotify borders flameshot
+brew install dropbox firefox keepassxc visual-studio-code discord slack steam alacritty vlc spotify borders flameshot telegram font-iosevka sf-symbols android-studio
 
 chmod +x /Applications/flameshot.app/Contents/MacOS/flameshot
+
+echo "https://color.firefox.com/?theme=XQAAAAIbAQAAAAAAAABBqYhm849SCia2CaaEGccwS-xMDPsqvOJTAr7MdSg-aIfxWLr1G9WC4LDSwLkx4w-id2jtOOMTunRBOZ722UBF6EvpdolmhlxmD3Or25T8oURi63VMsqda6LPDxPAVCtpokseuG-7zgywuccYqLcmbMinsEmMbl9u1Ho6VqTsj2mghJ82wuI84X8lEKKFlTTbQ1ZyMvSGKaTOUMDGnzD5aU5XH4DoQ6-EnaORmANQs0vSn0f_cHn4A"
