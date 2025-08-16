32a19f7beccfeb7a079f6b8ce8fc181f519ff3e3
Vítor Avelino <vitor@camarada.dev>
2025-08-16 18:22:44 -0300

    remove macOS files
    Signed-off-by: Vítor Avelino <vitor@camarada.dev>

--- sketchybar/items/clock/item.sh
+++
@@ -1,8 +0,0 @@
-CLOCK=(
-  update_freq=10
-  icon.drawing=off
-  script="$CONFIG_DIR/items/clock/script.sh"
-)
-
-sketchybar --add item clock e \
-           --set clock "${CLOCK[@]}"
--- sketchybar/items/date/item.sh
+++
@@ -1,7 +0,0 @@
-DATE=(
-  update_freq=10
-  script="$CONFIG_DIR/items/date/script.sh"
-)
-
-sketchybar --add item date q \
-           --set date "${DATE[@]}"
