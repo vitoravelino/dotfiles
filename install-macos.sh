f98fd6ad8baf8eea476a5ababff999e0fc3917e7
Vítor Avelino <vitor@camarada.dev>
2025-08-16 12:29:33 -0300

    update install-macos with more apps and settings
    Signed-off-by: Vítor Avelino <vitor@camarada.dev>

--- install-macos.sh
+++ install-macos.sh
@@ -17,7 +17,7 @@ rustup default stable
 # https://gist.github.com/phortuin/cf24b1cca3258720c71ad42977e1ba57
 
 # cli
-brew install fzf gnupg pinentry-mac htop bat wget ripgrep jq eza btop neofetch grep cloudflared watch btop neovim fd lazygit wget gnu-sed
+brew install fzf gnupg pinentry-mac htop bat wget ripgrep jq eza btop neofetch grep cloudflared watch btop neovim fd lazygit wget gnu-sed watchman tldr dust
 
 # language-based cli tools
 go install github.com/rhysd/dotfiles@latest
@@ -25,10 +25,17 @@ npm install -g pnpm yarn neovim diff-so-fancy eslint
 yarn create expo@next
 gem install neovim
 pip install -U yt-dlp httpie
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # casks
-brew install dropbox firefox keepassxc visual-studio-code discord slack steam alacritty vlc spotify borders flameshot telegram font-iosevka sf-symbols android-studio imageoptim latest orbstack arc zed obs
+brew install --cask keepassxc visual-studio-code legcord slack steam alacritty vlc spotify flameshot telegram font-iosevka sf-symbols android-studio imageoptim latest orbstack arc zed obs deskpad tailscale syncthing-app iina zen-browser cyberduck responsively ente-auth mullvad-vpn
 
 chmod +x /Applications/flameshot.app/Contents/MacOS/flameshot
 
 echo "https://color.firefox.com/?theme=XQAAAAIbAQAAAAAAAABBqYhm849SCia2CaaEGccwS-xMDPsqvOJTAr7MdSg-aIfxWLr1G9WC4LDSwLkx4w-id2jtOOMTunRBOZ722UBF6EvpdolmhlxmD3Or25T8oURi63VMsqda6LPDxPAVCtpokseuG-7zgywuccYqLcmbMinsEmMbl9u1Ho6VqTsj2mghJ82wuI84X8lEKKFlTTbQ1ZyMvSGKaTOUMDGnzD5aU5XH4DoQ6-EnaORmANQs0vSn0f_cHn4A"
+
+
+defaults write com.apple.screencapture location "~/Pictures"
+chflags nohidden ~/Library
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
