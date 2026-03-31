2f1be84185b97618addd2bd44980477131f478f8
Vítor Avelino <vitor@camarada.dev>
2026-03-31 06:41:24 -0300

    update aliases, exports, gitconfig and install
    Signed-off-by: Vítor Avelino <vitor@camarada.dev>

--- install.sh
+++ install.sh
@@ -77,63 +77,37 @@ check_os
 git clone https://github.com/vitoravelino/dotfiles.git
 cd $DOTFILES
 
-# repositories
-# keys
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo zypper --gpg-auto-import-keys ref
-# multimedia
-sudo zypper ar -c https://download.opensuse.org/repositories/multimedia:/apps/$REPO_OS_ID/multimedia:apps.repo
-# brave
-sudo zypper ar -c https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
-# codecs
-sudo zypper ar -c http://ftp.gwdg.de/pub/linux/misc/packman/suse/$REPO_OS_ID/ packman
-# vscode
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
-
-if [ $MODE == 'desktop' ]; then
-  # nvidia
-  sudo zypper ar -c https://download.nvidia.com/opensuse/$NVIDIA_REPO_OS_ID NVIDIA
-fi
-
-if [ $OS_VERSION = "opensuse-tumbleweed" ]; then
-  # lightdm webkit greeter
-  sudo zypper ar -c https://download.opensuse.org/repositories/home:antergos/$REPO_OS_ID/home:antergos.repo
-fi
-
-if [ $OS_VERSION = "opensuse-leap" ]; then
-  # polybar
-  sudo zypper ar -c https://download.opensuse.org/repositories/X11:/Utilities/$REPO_OS_ID/X11:Utilities.repo
-
-  # lightdm webkit greet, brightnessctl, etc
-  sudo zypper ar -c https://download.opensuse.org/repositories/home:vitoravelino_bkp/$LEAP_VERSION/home:vitoravelino_bkp.repo
-fi
-
 sudo zypper -n dup --allow-vendor-change --from packman
 
 # devel
 sudo zypper -n in --type pattern devel_basis
-sudo zypper -n in libopenssl-devel readline-devel libssh2-devel
-
-# i3lock-color deps
-sudo zypper -n in libev-devel xcb libxcb-devel xcb-util-image-devel xcb-util-devel xcb-util-xrm-devel xcb-util-wm-devel libxkbcommon-devel libxkbcommon-x11-devel cairo-devel libjpeg8-devel pam-devel xdpyinfo bc ImageMagick
-
-# qogir theme deps
-sudo zypper -n in gtk2-engines gtk2-engine-murrine
-
-# bspwm and cia
-sudo zypper -n in bspwm wmctrl scrot sxhkd rofi feh xdotool jq lightdm-webkit2-greeter picom dunst pamixer polybar
+sudo zypper -n in libopenssl-devel readline-devel libssh2-devel re2c sqlite3-devel libcurl-devel gd-devel oniguruma-devel postgresql18-server-devel libzip-devel
 
 # apps
 APPS='vlc vlc-codecs keepassxc dropbox hexchat libreoffice screenfetch sensors pulseaudio-equalizer htop inkscape optipng xdotool sshfs obs-studio obs-v4l2sink docker alacritty code discord flatpak wine lutris zsh bat nautilus pavucontrol redshift guvcview fortune gimp flash-player mpd ncmpcpp brave-browser steam'
 
-if [ $OS_VERSION = "opensuse-tumbleweed" ]; then
-  APPS+=' spotify-easyrpm'
-fi
-
-if [ $MODE == 'laptop' ]; then
-  APPS+=' pamixer brightnessctl'
-fi
+# winget upgrade
+# winget search glaze
+# winget upgrade Microsoft.DevHome
+# winget search uninstalr
+# winget install flux.flux
+# winget search keepass
+# winget search 7zip
+# winget search legcord
+# winget search nextcloud
+# winget search uget
+# winget search zen
+# winget search crystal
+# winget search cpuid
+# winget search corsair
+# winget search logitech
+# winget search epic
+# winget search zen
+# winget search winstall
+# winget search vim
+# winget install sharex
+# winget install ShareX.ShareX
+# winget install --exact --id MartiCliment.UniGetUI --source winget
 
 sudo zypper -n in $APPS
 
@@ -149,64 +123,19 @@ sudo flatpak install -y flathub org.onlyoffice
 sudo flatpak install -y flathub com.wps.Office
 sudo flatpak install -y flathub com.jetbrains.IntelliJ-IDEA-Community
 
-# lightdm
-sudo update-alternatives --set default-displaymanager /usr/lib/X11/displaymanagers/lightdm
-sudo cp -R $DOTFILES/common/webkit-greeter/build /usr/share/lightdm-webkit/themes/custom
-sudo sed -i 's/webkit_theme        = antergos/webkit_theme        = custom/g' /etc/lightdm/lightdm-webkit2-greeter.conf
-sudo cp $DOTFILES/common/01-my-lightdm.conf /usr/share/lightdm/lightdm.conf.d/
-
-# natural scrolling
-sudo ln -s $DOTFILES/common/X11/xorg.conf.d/25-natural-scrolling.conf /etc/X11/xorg.conf.d/25-natural-scrolling.conf
-
-if [ $MODE == 'laptop' ]; then
-  sudo ln -s $DOTFILES/laptop/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
-  sudo ln -s $DOTFILES/laptop/etc/profile.d/dpi.sh /etc/profile.d/dpi.sh
-fi
-
-if [ $MODE == 'desktop' ]; then
-  sudo ln -s $DOTFILES/desktop/X11/xorg.conf.d/20-nvidia.conf /etc/X11/xorg.conf.d/20-nvidia.conf
-fi
-
-# i3lock-color
-git clone https://github.com/Raymo111/i3lock-color.git
-cd $DOTFILES/i3lock-color
-chmod +x build.sh
-chmod +x install-i3lock-color.sh
-./build.sh
-./install-i3lock-color.sh
-cd $DOTFILES
-rm -rf $DOTFILES/i3lock-color
-
 # fzf
 FZF=$HOME/.fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git $FZF
 $FZF/install --key-bindings --completion --no-update-rc
 
-# asdf
-ASDF=$HOME/.asdf
-ln -s $DOTFILES/common/tool-versions $HOME/.tool-versions
-ASDF_BRANCH=$(get_latest_tag 'asdf-vm/asdf')
-git clone https://github.com/asdf-vm/asdf.git $ASDF --branch $ASDF_BRANCH
-. $ASDF/asdf.sh
+# mise
+curl https://mise.run | sh
+mise install
 
-asdf plugin-add crystal
-asdf plugin-add golang
-asdf plugin-add ruby
-asdf plugin-add erlang
-asdf plugin-add elixir
-asdf plugin-add nodejs
-asdf plugin-add python
-
-bash $ASDF/plugins/nodejs/bin/import-release-team-keyring
-asdf install
-
-asdf reshim
 npm -g install yarn
 pip install bpytop
 GO111MODULE=on go get github.com/rhysd/dotfiles
-asdf reshim
 yarn global add diff-so-fancy eslint
-asdf reshim
 
 # docker-compose
 pip install docker-compose
@@ -221,9 +150,6 @@ chsh -s /usr/bin/zsh
 
 # docker group
 sudo usermod -aG docker $USER
-
-# cursor size fix
-# sudo bash -c 'echo "Xcursor.size: 16" >> /etc/X11/Xresources'
 
 # symlink all dotfiles
 DOTFILES_REPO_PATH=$DOTFILES/common dotfiles link
@@ -261,16 +187,3 @@ echo -e "* Set OpenWeatherMap API key: ${YELLOW}echo KEY_VALUE > ~/.config/openw
 echo -e "* Change steam (wine) shortcut name ${YELLOW}~/.local/share/applications/wine/Programs/Steam/Steam.desktop${NC}"
 echo -e "* Set Firefox config ${YELLOW}ui.context_menus.after_mouseup${NC} to ${YELLOW}true${NC}"
 echo -e "* If Firefox Color is not restored, visit ${FIREFOX_COLOR}"
-
-if [ $OS_VERSION = "opensuse-leap" ]; then
-  echo -e "* Go to ${YELLOW}https://github.com/megamaced/spotify-easyrpm ${NC}and install it via 1-click yast install"
-fi
-
-if [ $MODE == 'laptop' ]; then
-  echo -e "* Download and install manually light from ${YELLOW}https://build.opensuse.org/package/show/home:OmniOBS/light${NC}"
-fi
-
-if [ $MODE == 'desktop' ]; then
-  echo -e "* Run ${YELLOW}sudo zypper -n in x11-video-nvidiaG05 ${NC}to install NVIDIA drivers"
-  echo -e "* And copy X11/xorg.conf.d/20-nvidia.conf to /etc"
-fi
